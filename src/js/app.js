7d07dc2d7b0e1f5c72e60597c5908660d7978e7e
Denis Sakharov <d.sakharov@ceteralabs.com>
2016-02-11 03:56:04 +0300

    новый шаблон верстки

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,32 @@
 ;$(document).foundation();
-function rotateCard(btn) {
-    var $card = $(btn).closest('.card-container');
-    console.log($card);
-    if ($card.hasClass('hover')) {
-        $card.removeClass('hover');
-    } else {
-        $card.addClass('hover');
-    }
-}
-alert(1);
+
+(function ($) {
+    "use strict";
+    $(function () {
+
+        $(".x-carousel-main").owlCarousel({
+            loop: true,
+            dots: false,
+            nav: true,
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            animateOut: 'fadeOut',
+            navText: ['<i class="fa fa-angle-left fa-3x"></i>', '<i class="fa fa-angle-right fa-3x"></i>']
+        });
+
+        $(".x-carousel-services").owlCarousel({
+            loop: true,
+            dots: true,
+            nav: false,
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            //animateOut: 'fadeOut',
+            navText: ['<i class="fa fa-angle-left fa-3x"></i>', '<i class="fa fa-angle-right fa-3x"></i>']
+        });
+
+    });
+})(jQuery);
