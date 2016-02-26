8fd7153b156354ae41338932cbccf50149298486
Denis Sakharov <d.sakharov@ceteralabs.com>
2016-02-26 16:24:02 +0300

    css inline

--- gulpfile.js
+++ gulpfile.js
@@ -31,15 +31,15 @@ var path = {
         html: 'src/jade/*.jade',
         js: 'src/jade/**/_js.html',
         jsHtml: 'dist/*.html',
-        css: 'src/scss/style.scss',
+        css: ['src/scss/style.scss', 'src/scss/style-inline.scss'],
         images: 'src/images/**/*.*',
         fonts: 'src/fonts/**/*.*',
         awesome: 'node_modules/font-awesome/fonts/**/*.*'
     },
     watch: {
-        html: ['src/jade/**/*.jade', 'src/jade/**/*.html'],
+        html: ['src/jade/**/*.jade', 'src/jade/**/*.html', 'src/scss/**/*.scss'],
         js: ['src/js/**/*.js', 'src/jade/**/_js.html'],
-        jsHtml: ['src/jade/**/*.jade', 'src/jade/**/*.html'],
+        jsHtml: ['src/jade/**/*.jade', 'src/jade/**/*.html', 'src/scss/**/*.scss'],
         css: 'src/scss/**/*.scss',
         images: 'src/images/**/*.*',
         fonts: 'src/fonts/**/*.*',
@@ -92,10 +92,11 @@ gulp.task('sass', ['clean-css'], function () {
         .pipe(gulp.dest(path.dist.css));
 });
 
-gulp.task('jade', ['clean-html'], function() {
+gulp.task('jade', ['clean-html', 'sass'], function() {
     return gulp.src(path.src.html)
         .pipe(jade({
-            pretty: true
+            pretty: true,
+            cache: true
         }))
         .pipe(gulp.dest(path.dist.html));
 });
